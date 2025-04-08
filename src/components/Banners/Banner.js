43080c9943fd52f7cb99b68b17d68b41655d73a6
sngmin <lsgmin@naver.com>
2025-04-08 13:48:31 +0900

    design: Banner Responsive Design

--- src/components/Banners/Banner.js
+++ src/components/Banners/Banner.js
@@ -11,23 +11,52 @@ export default function Banner() {
     const [visible, setVisible] = useState(true);
     if (!visible) return null;
     return (
-        <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+        <>
+            <div className="lg:hidden relative isolate flex  items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+                <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+                    <p className="text-xs text-gray-900">
+                        <strong className="font-semibold">{formatted}</strong>
+                        <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline size-0.5 fill-current">
+                            <circle r={1} cx={1} cy={1} />
+                        </svg>
+                        we’d love your feedback!
+                    </p>
+
+                    <a
+                        href="#"
+                        className="flex-none rounded-full bg-gray-900 px-2 py-0.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+                    >
+                        Feedback<span aria-hidden="true">&rarr;</span>
+                    </a>
+                </div>
+                <div className="flex flex-1 justify-end ">
+                    <button type="button"
+                            onClick={() => setVisible(false)}
+                            className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+                        <span className="sr-only">Dismiss</span>
+                        <XMarkIcon aria-hidden="true" className="size-5 text-gray-900" />
+                    </button>
+                </div>
+            </div>
+
+            <div className="lg:flex relative isolate hidden items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
             <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
-                <p className="text-sm/6 text-gray-900">
+                <p className="text-xs text-gray-900">
                     <strong className="font-semibold">{formatted}</strong>
                     <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline size-0.5 fill-current">
                         <circle r={1} cx={1} cy={1} />
                     </svg>
                     Our service is currently in testing — we’d love your feedback!
                 </p>
+
                 <a
                     href="#"
-                    className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+                    className="flex-none rounded-full bg-gray-900 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
                 >
                     Feedback<span aria-hidden="true">&rarr;</span>
                 </a>
             </div>
-            <div className="flex flex-1 justify-end">
+            <div className="flex flex-1 justify-end ">
                 <button type="button"
                         onClick={() => setVisible(false)}
                         className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
@@ -36,5 +65,33 @@ export default function Banner() {
                 </button>
             </div>
         </div>
+        </>
+
+        // <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+        //     <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+        //         <p className="text-xs text-gray-900">
+        //             <strong className="font-semibold">{formatted}</strong>
+        //             <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline size-0.5 fill-current">
+        //                 <circle r={1} cx={1} cy={1} />
+        //             </svg>
+        //             Our service is currently in testing — we’d love your feedback!
+        //         </p>
+        //
+        //         <a
+        //             href="#"
+        //             className="flex-none rounded-full bg-gray-900 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+        //         >
+        //             Feedback<span aria-hidden="true">&rarr;</span>
+        //         </a>
+        //     </div>
+        //     <div className="flex flex-1 justify-end ">
+        //         <button type="button"
+        //                 onClick={() => setVisible(false)}
+        //                 className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+        //             <span className="sr-only">Dismiss</span>
+        //             <XMarkIcon aria-hidden="true" className="size-5 text-gray-900" />
+        //         </button>
+        //     </div>
+        // </div>
     )
 }

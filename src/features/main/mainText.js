bdd4429771b9d27310ec99c586cba64ec5ada1a7
sngmin <lsgmin@naver.com>
2025-05-14 23:25:34 +0900

    Refactor: 메인텍스트 그라데이션 수정. #46

--- src/features/main/mainText.js
+++ src/features/main/mainText.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 export default function MainText() {
     return (
-        <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
-            <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
+        <section className="relative isolate overflow-hidden bg-white px-6 pb-12 sm:pt-10 sm:pb-14 lg:px-8">
+            <div className="absolute inset-0 -z-10 bg-[radial-gradient(20rem_8rem_at_top,theme(colors.indigo.100)_30%,white)] opacity-30" />
             <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
             <div className="mx-auto max-w-2xl lg:max-w-4xl">
 
-                <figure className="mt-10">
+                <figure className="mt-6">
                     <blockquote className="text-center text-xl/8 font-semibold text-gray-900 sm:text-2xl/9">
                         <p>
-                            “Add instant image analysis to your app—no complex AI deployment or infrastructure required with Gravifox API.”
+                            "Add instant image analysis to your app—no complex AI deployment or infrastructure required with Gravifox API."
                         </p>
                     </blockquote>
-                    <figcaption className="mt-10">
+                    <figcaption className="mt-8">
 
                         <div className="mt-4 flex items-center justify-center space-x-3 text-base">
                             <div className="font-semibold text-gray-900">GraviFox</div>
@@ -26,5 +26,5 @@ export default function MainText() {
                 </figure>
             </div>
         </section>
-    )
+    );
 }

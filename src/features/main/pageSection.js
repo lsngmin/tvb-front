c54aebd18d0336d0494853b1ebf25be1f22eea17
sngmin <lsgmin@naver.com>
2025-05-14 17:53:54 +0900

    Refactor: 그라데이션 영역, 색상 변경 #41

--- src/features/main/pageSection.js
+++ src/features/main/pageSection.js
@@ -4,15 +4,15 @@ export default function PageSection() {
             <div className="relative isolate px-6 mt-[-30px] lg:px-8">
                 <div
                     aria-hidden="true"
-                    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 pointer-events-none"
+                    className="absolute inset-x-0 top-20 -z-10 transform-gpu overflow-hidden blur-3xl sm:top-20 pointer-events-none"
                 >
-                    {/*<div*/}
-                    {/*    style={{*/}
-                    {/*        clipPath:*/}
-                    {/*            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',*/}
-                    {/*    }}*/}
-                    {/*    className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"*/}
-                    {/*/>*/}
+                    <div
+                        style={{
+                            clipPath:
+                                'polygon(20% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 20% 100%, 0% 80%, 0% 20%)',
+                        }}
+                        className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[12deg] bg-gradient-to-tr from-[#0ea5e9] to-[#7dd3fc] opacity-25 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                    />
                 </div>
                 <div className="mx-auto max-w-4xl py-16 sm:py-24 lg:py-32">
                     <div className="hidden sm:mb-8 sm:flex sm:justify-center">
@@ -34,16 +34,16 @@ export default function PageSection() {
                             Our endpoints handle the heavy lifting.
                         </p>
 
-                        <div className="mt-10 flex items-center justify-center gap-x-6">
+                        <div className="mt-10 flex items-center justify-center">
                             <a
                                 href="/free-trial"
-                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="rounded-lg bg-indigo-600 px-8 py-4 text-base font-bold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all duration-150"
                             >
                                 Get Started for Free
                             </a>
-                            <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                                Give Feedback <span aria-hidden="true">→</span>
-                            </a>
+                            {/*<a href="#" className="text-sm/6 font-semibold text-gray-900">*/}
+                            {/*    Give Feedback <span aria-hidden="true">→</span>*/}
+                            {/*</a>*/}
                         </div>
                     </div>
                 </div>
@@ -54,9 +54,9 @@ export default function PageSection() {
                     <div
                         style={{
                             clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                                'polygon(20% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 20% 100%, 0% 80%, 0% 20%)',
                         }}
-                        className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                        className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[-12deg] bg-gradient-to-tr from-[#0ea5e9] to-[#7dd3fc] opacity-25 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
                     />
                 </div>
             </div>

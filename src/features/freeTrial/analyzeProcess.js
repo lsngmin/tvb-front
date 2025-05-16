515614f7c6c37a8b4bef0b77e603097aae58febc
sngmin <lsgmin@naver.com>
2025-05-16 13:28:27 +0900

    feat: add FeatureCardGrid component for feature highlights. #34

--- src/features/freeTrial/analyzeProcess.js
+++ src/features/freeTrial/analyzeProcess.js
@@ -1,12 +1,12 @@
 import React from "react";
 import FirstStep from "./firstStep";
-
+import FeatureCardGrid from "./components/featureCardGrid";
 export default function analyzeProcess() {
 
     return (
         <div className="mt-2 mx-10">
             <div className="grid grid-cols-1 grid-rows-1 gap-10">
-                <div className="bg-blue-200 p-4">
+                <div className="">
                     <div className="items-center justify-center gap-2 mt-4">
                         <h2 className="text-gray-600 font-bold text-4xl tracking-tight text-center ">
                             Manipulated Image Detection
@@ -21,13 +21,7 @@ export default function analyzeProcess() {
                         <FirstStep/>
                     </div>
                 </div>
-                <div className="flex gap-4 items-center justify-center mt-4">
-                    <div className="rounded-full bg-gray-600 flex items-center justify-center">
-                        G
-                    </div>
-                    <div className="bg-gray-100 p-4">Item 2</div>
-                    <div className="bg-gray-100 p-4">Item 3</div>
-                </div>
+            <FeatureCardGrid/>
             </div>
         </div>
 

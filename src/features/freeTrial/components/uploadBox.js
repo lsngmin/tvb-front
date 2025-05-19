2ae6bfd17b02e31dcc486eb71390991c78914399
sngmin <lsgmin@naver.com>
2025-05-19 17:13:55 +0900

    refactor: 분석 결과 디자인 변경. #58

--- src/features/freeTrial/components/uploadBox.js
+++ src/features/freeTrial/components/uploadBox.js
@@ -4,12 +4,18 @@ import {useUpload} from "../provider/uploadProvider";
 import FileUploadAPI from "../api/fileUploadAPI";
 import StartAnalyzeButton from "./startAnalyzeButton";
 import Spinner from "./loadingSpinner";
+import FileAnalyzeAPI from "../api/fileAnalyzeAPI";
+import resultAnalyze from "./resultAnalyze";
+import ResultAnalyze from "./resultAnalyze";
 
 const UploadBox = () => {
     const {setUploadData} = useUpload();
     const {fileUpload} = FileUploadAPI();
     const [image, setImage] = useState();
     const [startAnalyze, setStartAnalyze] = useState(false);
+    const [endAnalyze, setEndAnalyze] = useState(false);
+    const [analyzeResult, setAnalyzeResult] = useState(false);
+    const {fileAnalyze} = FileAnalyzeAPI();
 
 
     /**
@@ -38,32 +44,57 @@ const UploadBox = () => {
         URL.revokeObjectURL(image)
         setImage(undefined);
     }
-    const handleStartAnalyze = () => {
+    const  handleStartAnalyze = async () => {
         setStartAnalyze(true)
+        const response = await fileAnalyze();//todo fileAnalyze에서 throw하는 error를 catch하는 로직이 필요합니다.
+        if (response?.status === 200) {
+            setAnalyzeResult(response.data)
+            setEndAnalyze(true)
+        } else {
+            //todo 에러 발생 시 예외처리 로직이 필요합니다.
+        }
     }
 
     return (
         <>
-            {image
-                ?
-                <div className={`max-w-sm sm:max-w-4xl mx-auto p-6 mt-10 rounded-2xl shadow-lg border border-gray-200`}>
-                    <div className="flex flex-col items-center justify-center h-64 gap-4">
-                        <img src={image} alt="Preview" className="cursor-pointer"/>
-                        {startAnalyze
-                            ?
-                            <Spinner/>
-                            :
-                            <StartAnalyzeButton resetImage = {handleResetImage} startAnalyze={handleStartAnalyze}/>
-                        }
-                    </div>
-
-                </div>
-                :
-                <div className={`max-w-sm sm:max-w-4xl mx-auto p-6 mt-10 rounded-2xl shadow-lg border border-gray-200`}>
-                    <FileUploader onUpload={handleFileChange} />
-                </div>
-
+            {
+                // image는 사진이 업로드 되면 사진 파일의 blob주소가 저장되는 변수입니다.
+                image
+                    ? (
+                        <div className="max-w-sm sm:max-w-4xl mx-auto p-6 mt-10 rounded-2xl shadow-lg border border-gray-200 bg-white">
+                            <div className="flex flex-col items-center justify-center h-auto gap-4">
+                                <img src={image} alt="Preview" className="cursor-pointer" />
+                                {/*
+                                    startAnalyze은 사용자가 start 버튼을 눌렀을 때 분석 과정을 시작하는 trigger입니다.
+                                    startAnalyze가 true이면 분석 중, false면 분석 전 상태입니다.
+                                  */}
+                                {startAnalyze
+                                    ? (
+                                        /*
+                                          endAnalyze는 분석이 끝났을 때 true로 바뀝니다.
+                                          분석 완료(endAnalyze === true)면 결과 컨테이너를, 아니면 Spinner를 보여줍니다.
+                                        */
+                                        endAnalyze
+                                            ? <ResultAnalyze analyzeResult={analyzeResult}/>
+                                            : <Spinner />
+                                    )
+                                    /*
+                                      startAnalyze가 false면 분석 시작 전이므로,
+                                      StartAnalyzeButton을 노출합니다.
+                                    */
+                                    : <StartAnalyzeButton resetImage={handleResetImage} startAnalyze={handleStartAnalyze} />
+                                }
+                            </div>
+                        </div>
+                    )
+                    : (
+                        /* image 값이 없으면 파일 업로드 컴포넌트만 보여줍니다. */
+                        <div className="max-w-sm sm:max-w-4xl mx-auto p-6 mt-10 rounded-2xl shadow-lg border border-gray-200 bg-white">
+                            <FileUploader onUpload={handleFileChange} />
+                        </div>
+                    )
             }
+
         </>
     );
 };

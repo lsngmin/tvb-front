7ae63bc57ef077d18f9065b9e17d3b7a13ba4e11
sngmin <lsgmin@naver.com>
2025-05-23 14:37:14 +0900

    feat: 프로필 API 연동. #25

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,20 +1,66 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import Navigation from "../features/navigation/navigation";
-import MainText from "features/main/mainText";
 import Footer from "../features/footer/footer";
-import Banner from "../features/banner/banner";
-import PageSection from "features/main/pageSection";
-import Feature from "features/main/feature";
-
+import profileAPI from "../features/profile/api/profileAPI";
 
 const Profile = () => {
+    const {fetchData} = profileAPI();
+    const [profileData, setProfileData] = useState({});
+
+    useEffect(() => {
+        const loadProfileData = async () => {
+            try {
+                const data = await fetchData();
+                setProfileData(data.data);  // 데이터를 상태로 설정
+                console.log(data.data);
+
+            } catch (error) {
+            }
+        };
+        loadProfileData();
+    }, []);
+
     return (
         <>
             <Navigation/>
-            <p>
-                사용자 프로필 페이지
-            </p>
+            <div className="max-w-7xl mx-auto mt-20">
+                <div className="px-4 sm:px-0">
+                    <h3 className="text-base/7 font-semibold text-gray-900">Your Profile</h3>
+                    <p className="mt-1 max-w-2xl text-sm/6 text-gray-500">Personal details and profile</p>
+                </div>
+
+                <div className="mt-6 border-t border-gray-100">
+                    <dl className="divide-y divide-gray-100">
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">Full name</dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">Margot Foster</dd>
+                        </div>
+                    </dl>
+                </div>
+
+                <div className="mt-16 px-4 sm:px-0">
+                    <h3 className="text-base/7 font-semibold text-gray-900">Account Settings</h3>
+                    <p className="mt-1 max-w-2xl text-sm/6 text-gray-500">Personal details and profile</p>
+                </div>
+
+                <div className="mt-6 border-t border-gray-100">
+                    <dl className="divide-y divide-gray-100">
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">Change Password</dt>
+                            <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+
+                                <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
+                                    <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm/6">
+                                    </li>
+                                    <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm/6">
+                                    </li>
+                                </ul>
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
             <Footer />
         </>
     );
